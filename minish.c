cda7884366973c74a9eb9b831ff02d1abd01943b
Andres <gokutanve@gmail.com>
2026-02-01 18:24:33 -0400

    se agrego verificacionpara ver si el archivo o directorio es oculto, para asi omitirlo. Se corrigio identacion

--- minish.c
+++ minish.c
@@ -13,7 +13,7 @@ void parse_buffer(char buffer[], char *mi_argv[]);
 void mi_cd(char *mi_argv[], char *home);
 void mi_pwd();
 void mi_ls(int p);
-void mi_stat();
+void mi_stats();
 void imprimir_fecha_formateada(time_t t);
 void mi_help();
 int directorio_relativo(char *ruta);
@@ -33,6 +33,7 @@ int main(int argc, char *argv[], char *envp[]){
         if (bytes_leidos > 0){
             buffer[bytes_leidos - 1] = '\0';
             parse_buffer(buffer, mi_argv);
+
             if (mi_argv[0] == NULL){
                 continue;
             }
@@ -45,9 +46,11 @@ int main(int argc, char *argv[], char *envp[]){
                 mi_pwd();
             } else if (strcmp(mi_argv[0], "ls") == 0){
                 int p = 0;
+
                 if (mi_argv[1] != NULL && strcmp(mi_argv[1], "-1") == 0){
                     p = 1;
                 }
+
                 mi_ls(p);
             } else if(strcmp(mi_argv[0], "stats") == 0){
                 mi_stats();
@@ -64,17 +67,17 @@ int main(int argc, char *argv[], char *envp[]){
 
 char *obtener_home(char *envp[]){
     char *home;
-
     int i = 0;
+
     while (envp[i] != NULL){
         if (envp[i][0] == 'H' && envp[i][1] == 'O' && envp[i][2] == 'M' &&
             envp[i][3] == 'E' && envp[i][4] == '='){
             home = &envp[i][5];
             break;
         }
-
         i++;
     }
+
     return home;
 }
 
@@ -110,12 +113,15 @@ void mi_cd(char *mi_argv[], char *home){
         chdir(home);
         return;
     }
+
     if (chdir(mi_argv[1]) != 0){
         write(2, "Error: No se encontro el directorio \n", 38);
         int length = 0;
+
         while (mi_argv[1][length] != '\0'){
             length++;
         }
+
         write(2, mi_argv[1], length);
         write(2, "\n", 1);
     }
@@ -123,11 +129,14 @@ void mi_cd(char *mi_argv[], char *home){
 
 void mi_pwd(){
     char path[1024];
+
     if (getcwd(path, sizeof(path)) != NULL){
         int length = 0;
+
         while (path[length] != '\0'){
             length++;
         }
+
         write(1, path, length);
         write(1, "\n", 1);
     }
@@ -136,27 +145,38 @@ void mi_pwd(){
 void mi_ls(int p){
     DIR *dir = opendir(".");
     struct stat info;
+
     if (dir == NULL) {
         write(2, "Error al abrir el directorio\n", 29);
     } else {
         struct dirent *entrada;
+
         while ((entrada = readdir(dir)) != NULL) {
             int len_file = 0;
+
+            if (entrada->d_name[0] == '.'){
+                continue;
+            }
+
             while (entrada->d_name[len_file] != '\0'){
                 len_file++;
             } 
+
             if (p == 0){
                 write(1, entrada->d_name, len_file);
                 write(1, " ", 1);
             } else {
                 stat(entrada->d_name, &info);
+
                 if (entrada->d_type == DT_DIR){
                     write(1, "Directorio ", 12);
                 } else if (entrada->d_type == DT_REG){
                     write(1, "Archivo ", 8);
                 }
+
                 char size_str[20];
                 int n = sprintf(size_str, "%ld", info.st_size);
+
                 write(1, size_str, n);
                 write(1, " ", 1);
                 imprimir_fecha_formateada(info.st_mtime);
@@ -173,7 +193,6 @@ void mi_ls(int p){
 
 void imprimir_fecha_formateada(time_t t) {
     char buffer_fecha[20]; 
-    
 
     struct tm *tm_info = localtime(&t);
     strftime(buffer_fecha, sizeof(buffer_fecha), "%Y%m%d %H:%M", tm_info);
@@ -183,6 +202,7 @@ void imprimir_fecha_formateada(time_t t) {
 
 void mi_stats(){
     DIR *dir = opendir(".");
+
     if (dir == NULL) {
         write(2, "Error al abrir el directorio\n", 29);
     } 
@@ -192,12 +212,17 @@ void mi_stats(){
     int k = 0;
 
     while ((entrada = readdir(dir)) != NULL) {
+        if(entrada->d_name[0] == '.'){
+            continue;
+        }
+        
         if (entrada->d_type == DT_DIR){
             i++;
         } else if (entrada->d_type == DT_REG){
             k++;
         }
     }
+
     closedir(dir);
     char i_str[12];
     char k_str[12];
